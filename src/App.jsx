367989a4e2160fcfacf92244c85f75e7f0735a4e
RAGHUPATHIREDDY1 <raghuvija330@gmail;.com>
2026-05-09 23:05:49 +0530

    I updated the file name now app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter,  Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
+import Home from './components/home'
 import NotFound from './components/NotFound'
 import Login from './components/login'
 import Popular from './components/popular'
