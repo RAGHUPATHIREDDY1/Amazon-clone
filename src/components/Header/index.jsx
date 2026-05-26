59dd38f135686feb9bf2687da1a219f332dc5bee
RAGHUPATHIREDDY1 <raghuvija330@gmail;.com>
2026-05-26 16:01:08 +0530

    i have added the logout button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,17 @@
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import Cookies from 'js-cookie';
 import Avatar from '../../assets/avatar.png';
 import './index.css';
+
 const Header = () => {
+  const navigate = useNavigate();
+  const jwtToken = Cookies.get('jwt_token');
+
+  const onClickLogout = () => {
+    Cookies.remove('jwt_token');
+    navigate('/login');
+  };
+
   return (
     <div className="header-container">
         <nav className="header-nav">
@@ -11,9 +21,16 @@ const Header = () => {
                 <li><Link to="/popular">Popular</Link></li>
            </ul>
            <ul className="header-search-avatar">
-           <Link to="/login">
-             <img src={Avatar} alt="Avatar" className="header-avatar" />
-           </Link>
+           {jwtToken === undefined ? (
+             <Link to="/login">
+               <img src={Avatar} alt="Avatar" className="header-avatar" />
+             </Link>
+           ) : (
+             <>
+               <img src={Avatar} alt="Avatar" className="header-avatar" />
+               <button type="button" className="logout-button" onClick={onClickLogout}>Logout</button>
+             </>
+           )}
              </ul>
         </nav>
     </div>
